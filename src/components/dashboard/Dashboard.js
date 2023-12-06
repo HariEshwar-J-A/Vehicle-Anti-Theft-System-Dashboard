aaae309076e9c637207b6d04d351d3f337d887d9
Harieshwar J A <harirmdeee@gmail.com>
2023-12-06 17:12:30 -0500

    feat(dashboard): :sparkles: add data based rendering for all components in dashboard

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,77 +1,108 @@
 import React, { useEffect, useState } from 'react'
 import styles from './Dashboard.module.scss'
 
-const Dashboard = () => {
+const Dashboard = ({
+  currentState,
+  isBrakeOverridden,
+  isAlarmOn,
+  isEngineOn,
+  timeStamp,
+  rawData,
+  userName,
+  userPresence
+}) => {
+
+
   return (
     <div className={styles.dashboard}>
-      <DriverPresenceIndicator />
-      <DriverAuthenticatedIndicator />
-      <AlarmIndicator/>
-      <CurrentStateIndicator />
+      <DriverPresenceIndicator userPresence={userPresence} />
+      <DriverAuthenticatedIndicator userName={userName}/>
+      <AlarmIndicator isAlarmOn={isAlarmOn}/>
+      <CurrentStateIndicator currentState={currentState}/>
+      <EngineOnIndicator isEngineOn={isEngineOn}/>
+      <BrakeOverRiddenIndicator isBrakeOverridden={isBrakeOverridden}/>
     </div>
   )
 }
 
-const DriverPresenceIndicator = () => {
-  const [isPresent, setIsPresent] = useState(true)
+const DriverPresenceIndicator = ({userPresence}) => {
 
   return (
     <div className={styles.presenceIndicator}>
       <div className={styles.indicatorLabel}>Human in Car:</div>
       <div
-        className={`${styles.led} ${isPresent ? styles.turnedOn : ''}`}
+        className={`${styles.led} ${userPresence ? styles.turnedOn : ''}`}
       ></div>
     </div>
   )
 }
 
-const CurrentStateIndicator = () => {
-  const [currentState, setCurrentState] = useState('Drive Mode')
+const CurrentStateIndicator = ({currentState}) => {
 
-  const turnOnLockMode = () => setCurrentState('Lock Mode')
-  const turnOnDriveMode = () => setCurrentState('Drive Mode')
-  const turnOnShutdownMode = () => setCurrentState('Shutdown Mode')
 
   return (
     <div className={styles.statusIndicator}>
-      <div className={styles.currentState}>
-        Current State:
-      </div>
+      <div className={styles.currentState}>Current State:</div>
       <div className={styles.currentStateValue}>{currentState}</div>
     </div>
   )
 }
 
-const DriverAuthenticatedIndicator = () => {
-    const [isDriverAuthenticated, setIsDriverAuthenticated] = useState(true);
-
-    return (
-        <div className={styles.presenceIndicator}>
-          <div className={styles.indicatorLabel}>Driver Authenticated: </div>
-          <div
-            className={`${styles.led} ${isDriverAuthenticated ? styles.turnedOn : ''}`}
-          ></div>
-        </div>
-      )
+const DriverAuthenticatedIndicator = ({userName}) => {
+  return (
+    <div className={styles.presenceIndicator}>
+      <div className={styles.indicatorLabel}>Driver Authenticated: </div>
+      <div
+        className={`${styles.led} ${
+          userName !== "None" ? styles.turnedOn : ''
+        }`}
+      ></div>
+    </div>
+  )
 }
 
-const AlarmIndicator = () => {
-    const [isAlarmOn, setIsAlarmOn] = useState(false);
+const EngineOnIndicator = ({isEngineOn}) => {
+  return (
+    <div className={styles.presenceIndicator}>
+      <div className={styles.indicatorLabel}>Driver Authenticated: </div>
+      <div
+        className={`${styles.led} ${
+          isEngineOn ? styles.turnedOn : ''
+        }`}
+      ></div>
+    </div>
+  )
+}
 
-    useEffect(() => {
-        if (isAlarmOn) {
-            const notify = new Notification("Alarm Activated, Check your Car!")
-        }
-    }, [isAlarmOn])
+const BrakeOverRiddenIndicator = ({isBrakeOverridden}) => {
+  return (
+    <div className={styles.presenceIndicator}>
+      <div className={styles.indicatorLabel}>Driver Authenticated: </div>
+      <div
+        className={`${styles.led} ${
+          isBrakeOverridden ? styles.turnedOn : ''
+        }`}
+      ></div>
+    </div>
+  )
+}
 
-    return (
-        <div className={styles.presenceIndicator}>
-          <div className={styles.indicatorLabel}>Is Alarm Turned On: </div>
-          <div
-            className={`${styles.led} ${isAlarmOn ? styles.turnedOn : ''}`}
-          ></div>
-        </div>
-      )
+const AlarmIndicator = ({isAlarmOn}) => {
+
+  useEffect(() => {
+    if (isAlarmOn) {
+      const notify = new Notification('Alarm Activated, Check your Car!')
+    }
+  }, [isAlarmOn])
+
+  return (
+    <div className={styles.presenceIndicator}>
+      <div className={styles.indicatorLabel}>Is Alarm Turned On: </div>
+      <div
+        className={`${styles.led} ${isAlarmOn ? styles.turnedOn : ''}`}
+      ></div>
+    </div>
+  )
 }
 
 export default Dashboard
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,18 +1,110 @@
-import React from 'react';
-import Header from '../header/Header';
-import Footer from '../footer/Footer';
-import Dashboard from '../dashboard/Dashboard';
+import React, { useState, useEffect } from 'react'
+import { usePubNub } from 'pubnub-react'
+import Header from '../header/Header'
+import Footer from '../footer/Footer'
+import Dashboard from '../dashboard/Dashboard'
+import Logs from '../logs/Logs'
 
-import styles from './Home.module.scss';
+import Button from '@mui/material/Button'
+
+import styles from './Home.module.scss'
 
 const Home = () => {
+  const [isLogsOpen, setIsLogsOpen] = useState(true)
+
+  const toggleLogsWindow = () => setIsLogsOpen(prevState => !prevState)
+
+  const pubnub = usePubNub()
+  const [channels] = useState([process.env.REACT_APP_CHANNEL])
+  const [messages, addMessage] = useState([])
+  const [currentState, setCurrentState] = useState('Lock Mode');
+  const [isBrakeOverridden, setIsBrakeOverridden] = useState(false);
+  const [isAlarmOn, setIsAlarmOn] = useState(false);
+  const [isEngineOn, setIsEngineOn] = useState(false);
+  const [timeStamp, setTimeStamp] = useState('');
+  const [rawData, setRawData] = useState({});
+  const [userName, setUserName] = useState('None');
+  const [userPresence, setUserPresence] = useState(false);
+
+  const handleMessage = event => {
+    const message = event.message
+    console.log(typeof message)
+
+    if (typeof message === 'string' || message.hasOwnProperty('messageType')) {
+      
+      const jsonMessage = JSON.parse(message);
+      console.log(jsonMessage)
+      
+      const messageType = jsonMessage.messageType;
+      const receivedCurrentState = jsonMessage.currentState;
+      const receivedIsBrakeOverridden = jsonMessage.isBrakeOverridden;
+      const receivedIsAlarmOn = jsonMessage.isAlarmOn;
+      const receivedIsEngineOn = jsonMessage.isEngineOn;
+      const receivedTimeStamp = jsonMessage.timeStamp;
+      const receivedRawData = jsonMessage.rawData;
+      const receivedUserName = jsonMessage.userName;
+      const receivedUserPresence = jsonMessage.userPresence;
+      
+      switch (messageType) {
+        case 'state_update':
+          setCurrentState(receivedCurrentState)
+          setIsBrakeOverridden(receivedIsBrakeOverridden)
+          setIsAlarmOn(receivedIsAlarmOn)
+          setIsEngineOn(receivedIsEngineOn)
+          setTimeStamp(receivedTimeStamp)
+          setRawData(receivedRawData)
+          break
+        case 'sensor_update':
+          setUserName(receivedUserName)
+          setUserPresence(receivedUserPresence)
+          setTimeStamp(receivedTimeStamp)
+          setRawData(receivedRawData)
+          break
+        default:
+          break
+      }
+    }
+
+    if (typeof message === 'string' || message.hasOwnProperty('text')) {
+      const text = message.text || message
+      addMessage(messages => [...messages.slice(0, 99), text])
+    }
+  }
+
+  useEffect(() => {
+    const listenerParams = { message: handleMessage }
+    pubnub.addListener(listenerParams)
+    pubnub.subscribe({ channels })
+    return () => {
+      pubnub.unsubscribe({ channels })
+      pubnub.removeListener(listenerParams)
+    }
+  }, [pubnub, channels])
+
   return (
     <div className={styles.home}>
-      <Header/>
-      <Dashboard/>
-      <Footer/>
+      <Header />
+      <Button
+        className={`${styles.logButton} ${isLogsOpen ? styles.turnedOn : ''}`}
+        variant='contained'
+        onClick={toggleLogsWindow}
+      >
+        Logs
+      </Button>
+      {isLogsOpen && <Logs messages={messages} />}
+      <Dashboard 
+      currentState={currentState}
+      isBrakeOverridden={isBrakeOverridden}
+      isAlarmOn={isAlarmOn}
+      isEngineOn={isEngineOn}
+      timeStamp={timeStamp}
+      rawData={rawData}
+      userName={userName}
+      userPresence={userPresence} 
+      />
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default Home;
+export default Home
